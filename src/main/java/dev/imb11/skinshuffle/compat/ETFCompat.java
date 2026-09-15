cb96e3eaa5aac27f8a246f4ebd1d7fc2e631ea12
Cassian <cassian@godsted.com>
2026-09-15 10:20:31 -0400

    Update ETFCompat.java

--- src/main/java/dev/imb11/skinshuffle/compat/ETFCompat.java
+++ src/main/java/dev/imb11/skinshuffle/compat/ETFCompat.java
@@ -1,9 +1,7 @@
 package dev.imb11.skinshuffle.compat;
 
-import traben.entity_texture_features.features.ETFRenderContext;
-
 public class ETFCompat {
     public static void preventRenderLayerIssue() {
-        ETFRenderContext.preventRenderLayerTextureModify();
+//        ETFRenderContext.preventRenderLayerTextureModify();
     }
 }
